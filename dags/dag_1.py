47805a98addcefb0ddd77646c1bf686a55601787
Ignacio Bascuñán <116374140+IgnaBascu@users.noreply.github.com>
2024-03-19 11:54:59 -0300

    Add files via upload

---
+++ dags/dag_1.py
@@ -0,0 +1,72 @@
+import pprint
+import requests
+import json
+
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from confluent_kafka import Producer
+
+
+def fetch_weather_data():
+    try:
+        # Realiza la solicitud a la API de clima (Coordenadas Santiago, Chile)
+        api_url = 'https://api.openweathermap.org/data/2.5/weather?lat=-33.457&lon=-70.648&appid=a03874207feb68965f8ca246322fdfcf'
+        response = requests.get(api_url)
+        weather_data = response.json()
+        return weather_data
+    except Exception as e:
+        print(f"Error al obtener datos del clima: {str(e)}")
+        return None
+    
+def print_json(**kwargs):
+    ti = kwargs['ti']
+    json_data = ti.xcom_pull(task_ids='fetch_weather_task')  # Obtener el JSON de la tarea anterior
+    if json_data:
+        pprint.pprint(json_data)  # Imprimir el JSON utilizando pprint
+    else:
+        print("No se pudo obtener el JSON de datos del clima")
+
+def json_serialization(**kwargs):
+    ti = kwargs['ti']
+    json_data = ti.xcom_pull(task_ids='fetch_weather_task')  # Obtener el JSON de la tarea anterior
+    if json_data:
+        producer_config = {
+            'bootstrap.servers': 'localhost:9092',
+            'client.id': 'airflow'
+        }
+        producer = Producer(producer_config)
+        json_message = json.dumps(json_data)
+        producer.produce('airflow-spark', value=json_message)
+        producer.flush()
+    else:
+        print("No se pudo obtener el JSON de datos del clima")
+
+dag = DAG (
+        "DAG_API_OPENWEATHER",
+        schedule_interval=timedelta(seconds=5),
+        start_date=datetime(2024, 3, 19)
+    )
+
+task1 = PythonOperator(
+    task_id='fetch_weather_task',
+    python_callable=fetch_weather_data,
+    dag=dag,
+    provide_context=True,
+)
+
+task2 = PythonOperator(
+    task_id='get_json_from_task1',
+    python_callable=print_json,
+    dag=dag,
+    provide_context=True,
+)
+
+task3 = PythonOperator(
+    task_id='run_kafka_producer',
+    python_callable=json_serialization,
+    dag=dag,
+    provide_context=True,
+)
+
+task1 >> task2 >> task3
